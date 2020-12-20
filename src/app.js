385ddb91482083b7f14a530f367a26ee42e754f5
Eslim Daga <esliim9811@gmail.com>
2020-12-20 18:49:30 -0500

    Configured to product routes

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+import pkg from "../package.json";
+import ProductsRoute from "./routes/Products.routes";
+
+const app = express();
+
+app.set("pkg", pkg);
+
+app.use(morgan("dev"));
+
+app.get("/",(req,res) => {
+  res.json({
+    name : app.get("pkg").name,
+    author : app.get("pkg").author,
+    description : app.get("pkg").description,
+    version : app.get("pkg").version
+  });
+});
+
+app.use("/products", ProductsRoute);
+
+export default app;
