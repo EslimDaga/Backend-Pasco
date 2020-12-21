ba607d1eff2db72bdd5e4636a3bda660182e8178
Eslim Daga <esliim9811@gmail.com>
2020-12-20 22:04:36 -0500

    Add useFindAndModify

--- src/database.js
+++ src/database.js
@@ -2,7 +2,8 @@ import moongose from "mongoose";
 
 moongose.connect("mongodb://localhost/pasco-db",{
     useNewUrlParser : true,
-    useUnifiedTopology : true
+    useUnifiedTopology : true,
+    useFindAndModify : true
   })
   .then(db => console.log("Database is Connected"))
   .catch(error => console.log(error));
