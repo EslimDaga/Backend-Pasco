0051703277c50420c8419c774a14e31e32269390
Eslim Daga <esliim9811@gmail.com>
2020-12-20 20:27:43 -0500

    Read and Add products finish

--- src/controllers/Product.controller.js
+++ src/controllers/Product.controller.js
@@ -1,9 +1,15 @@
-export const createProduct = (req,res) => {
+import Product from "../models/Product";
 
+export const createProduct = async(req,res) => {
+  const { name, category, price, imgURL } = req.body;
+  const newProduct = new Product({ name,category,price,imgURL });
+  const productSaved = await newProduct.save();
+  res.status(201).json(productSaved);
 }
 
-export const getProducts = (req,res) => {
-  res.json("Get Products")
+export const getProducts = async(req,res) => {
+  const products = await Product.find()
+  res.json(products);
 }
 
 export const getProductById = (req,res) => {
