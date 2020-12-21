abece543d1a51473391746012156775bde756f37
Eslim Daga <esliim9811@gmail.com>
2020-12-20 21:34:18 -0500

    Add Update and Delete products

--- src/controllers/Product.controller.js
+++ src/controllers/Product.controller.js
@@ -5,21 +5,27 @@ export const createProduct = async(req,res) => {
   const newProduct = new Product({ name,category,price,imgURL });
   const productSaved = await newProduct.save();
   res.status(201).json(productSaved);
-}
+};
 
 export const getProducts = async(req,res) => {
   const products = await Product.find()
   res.json(products);
-}
+};
 
-export const getProductById = (req,res) => {
+export const getProductById = async(req,res) => {
+  const product = await Product.findById(req.params.productId);
+  res.status(200).json(product);
+};
 
-}
+export const updateProductById = async(req,res) => {
+  const updateProduct = await Product.findByIdAndUpdate(req.params.productId, req.body, {
+    new : true
+  });
+  res.status(200).json(updateProduct);
+};
 
-export const updateProductById = (req,res) => {
-
-}
-
-export const deleteProductById = (req,res) => {
-
+export const deleteProductById = async(req,res) => {
+  const { productId } = req.params;
+  await Product.findByIdAndDelete(productId);
+  res.status(204).json("Product delete");
 }
