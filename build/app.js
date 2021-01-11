7495ecf839aced7e1589f6f582fd3e894e3f25b0
Segursat-Peru <edaga@segursat.com>
2021-01-11 14:36:45 -0500

    Production code

---
+++ build/app.js
@@ -0,0 +1,34 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = _interopRequireDefault(require("express"));
+
+var _morgan = _interopRequireDefault(require("morgan"));
+
+var _package = _interopRequireDefault(require("../package.json"));
+
+var _district = _interopRequireDefault(require("./routes/district.routes"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var app = (0, _express["default"])(); //Settings
+
+app.set("port", process.env.PORT || 4000);
+app.set("pkg", _package["default"]); //Middleware
+
+app.use((0, _morgan["default"])("dev"));
+app.use(_district["default"]);
+app.get("/", function (req, res) {
+  res.json({
+    name: app.get("pkg").name,
+    author: app.get("pkg").author,
+    description: app.get("pkg").description,
+    version: app.get("pkg").version
+  });
+});
+var _default = app;
+exports["default"] = _default;
--- build/index.js
+++ build/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var _express = _interopRequireDefault(require("express"));
+var _app = _interopRequireDefault(require("./app"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var app = (0, _express["default"])();
-app.listen(3000);
-console.log("Server listen on Port", 3000);
+_app["default"].listen(_app["default"].get("port"));
+
+console.log("Server listen on Port", _app["default"].get("port"));
