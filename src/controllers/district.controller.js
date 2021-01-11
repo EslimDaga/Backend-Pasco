4879463eb525eb08d527efc8f34ce4b81eded8da
Segursat-Peru <edaga@segursat.com>
2021-01-11 16:07:04 -0500

    Create routes the project

---
+++ src/controllers/district.controller.js
@@ -0,0 +1,19 @@
+export const getDistrict = (req,res) => {
+  res.json("Get district")
+}
+
+export const getDistrictById = (req,res) => {
+
+}
+
+export const createDistrict = (req,res) => {
+
+}
+
+export const updateDistrict = (req,res) => {
+
+}
+
+export const deleteDistrict = (req,res) => {
+
+}
