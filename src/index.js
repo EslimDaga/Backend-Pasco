0bcbbf0fb100cf6e8bf2d2da8fb4b40d0a2b66d9
Segursat-Peru <edaga@segursat.com>
2021-01-11 12:51:47 -0500

    Test ready Heroku deploy

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Works");
+import express from "express";
+
+const app = express();
+
+app.set('port', process.env.PORT || 4000);
+
+app.listen(app.get('port'));
+console.log("Server listen on Port",app.get('port'));
