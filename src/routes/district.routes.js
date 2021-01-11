1a0e6c7205e708c91b911f6b8d25fda32905d537
Segursat-Peru <edaga@segursat.com>
2021-01-11 16:09:19 -0500

    Add crud routes

--- src/routes/district.routes.js
+++ src/routes/district.routes.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
+import * as districtController from "../controllers/district.controller";
+
 const router = Router();
 
-router.get("/disctrict", (req,res) => {
-  res.json("Get disctricts");
-});
+router.get("/all", districtController.getDistrict);
+
+router.post("/", districtController.createDistrict);
+
+router.get("/:districtId", districtController.getDistrictById);
+
+router.put("/:districtId", districtController.updateDistrict);
+
+router.delete("/:districtId", districtController.deleteDistrict);
+
 
 export default router;
