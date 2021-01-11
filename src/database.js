4bd2cccfaff8e2e398e5ed39df020f86ddd40bd1
Segursat-Peru <edaga@segursat.com>
2021-01-11 16:43:56 -0500

    Fix database

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 mongoose.connect("mongodb+srv://edaga:daga2020@cluster0.qxixk.mongodb.net/<dbname>?retryWrites=true&w=majority", {
   useNewUrlParser : true,
-  useUnifiedTopology : true
-})
-  .then(db => console.log("Database is connected"))
+  useUnifiedTopology : true,
+  useCreateIndex : true
+}).then(db => console.log("Database is connected"))
   .catch(error => console.error(error));
