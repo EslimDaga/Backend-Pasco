088012b78034ef7bdec6dc6b9e807a2d2e420466
Segursat-Peru <edaga@segursat.com>
2021-01-11 17:11:13 -0500

    Change name database

--- src/controllers/district.controller.js
+++ src/controllers/district.controller.js
@@ -1,15 +1,18 @@
 import District from "../models/District";
 
 export const getDistrict = (req,res) => {
-
+  res.json("Get all District");
 }
 
 export const getDistrictById = (req,res) => {
-  res.json("Creating Product")
+  res.json("Get by id district");
 }
 
-export const createDistrict = (req,res) => {
-
+export const createDistrict = async(req,res) => {
+  const { name, region, province, latitude, longitude, altitude, imagesUrl } = req.body;
+  const newDistrict = new District({ name, region, province, latitude, longitude, altitude, imagesUrl });
+  const districtSaved = await newDistrict.save();
+  res.status(201).json(districtSaved);
 }
 
 export const updateDistrict = (req,res) => {
