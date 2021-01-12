1e1a336d618c8f3f474560589060f07b7a516399
Eslim Daga <esliim9811@gmail.com>
2021-01-12 14:27:35 -0500

    Add getById, update and delete

--- src/controllers/district.controller.js
+++ src/controllers/district.controller.js
@@ -5,8 +5,9 @@ export const getDistrict = async(req,res) => {
   res.json(districts);
 }
 
-export const getDistrictById = (req,res) => {
-  res.json("Get by id district");
+export const getDistrictById = async(req,res) => {
+  const district = await District.findById(req.params.districtId);
+  res.status(200).json(district);
 }
 
 export const createDistrict = async(req,res) => {
@@ -16,10 +17,15 @@ export const createDistrict = async(req,res) => {
   res.status(201).json(districtSaved);
 }
 
-export const updateDistrict = (req,res) => {
-
+export const updateDistrict = async(req,res) => {
+  const updateDistrict = await District.findByIdAndUpdate(req.params.districtId, req.body, {
+    new : true
+  });
+  res.status(200).json(updateDistrict);
 }
 
-export const deleteDistrict = (req,res) => {
-
+export const deleteDistrict = async(req,res) => {
+  const { districtId } = req.params;
+  await District.findByIdAndDelete(districtId);
+  res.status(204).json();
 }
