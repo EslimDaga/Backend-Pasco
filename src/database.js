7dfaafdf603800c57d322113a996bbc7ccee3a85
Eslim Daga <esliim9811@gmail.com>
2021-01-13 23:00:30 -0500

    Add useFindAndModify

--- src/database.js
+++ src/database.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 mongoose.connect("mongodb+srv://edaga:daga2020@cluster0.qxixk.mongodb.net/pascodatabase?retryWrites=true&w=majority", {
   useNewUrlParser : true,
   useUnifiedTopology : true,
+  useFindAndModify : true,
   useCreateIndex : true
 }).then(db => console.log("Database is connected"))
   .catch(error => console.error(error));
