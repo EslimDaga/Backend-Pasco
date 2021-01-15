9b40e0697aa6546954816c3af50e74d4fe36fd3c
Eslim Daga <esliim9811@gmail.com>
2021-01-14 21:50:02 -0500

    Add role configuration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/User";
 import jwt from "jsonwebtoken";
 import config from "../config";
+import Role from "../models/Role";
 
 export const signUp = async(req,res) => {
   const { username, email, password, roles } = req.body;
@@ -11,7 +12,16 @@ export const signUp = async(req,res) => {
     password : await User.encryptPassword(password)
   });
 
+  if (roles) {
+    const foundDoles = await Role.find({ name : {$in : roles} })
+    newUser.roles = foundDoles.map(role => role._id);
+  }else{
+    const role = await Role.findOne({ name : "user"});
+    newUser.roles = [role._id];
+  }
+
   const savedUser = await newUser.save();
+  console.log(savedUser);
 
   const token = jwt.sign({ id : savedUser._id }, config.SECRET, {
     expiresIn :  86400 //24 hours
@@ -21,5 +31,15 @@ export const signUp = async(req,res) => {
 };
 
 export const signIn = async(req,res) => {
-  res.json("signin");
+  const userFound = await User.findOne({ email : req.body.email }).populate("roles");
+  if (!userFound) return res.json({ message : "User not found" });
+
+  const matchPassword = await User.comparePassword(req.body.password, userFound.password);
+  if(!matchPassword) return res.status(401).json({ token : null, message : "Invalid password" });
+
+  const token = jwt.sign({ id : userFound._id}, config.SECRET, {
+    expiresIn : 86400
+  });
+
+  res.json({ token })
 };
